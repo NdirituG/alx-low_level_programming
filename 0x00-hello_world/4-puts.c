9be528922b238af97e05db17ab887c1276231a99
NdirituG <denisndiritug@gmail.com>
2022-04-07 16:24:36 -0700

    final edit for pus

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,20 @@
 /*
- * selecta shong'e
- * this one uses puts function
+ * File: 4-puts.c
+ * a code for puts function
  */
 
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 }

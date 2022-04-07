d1b96fc3abe0c8af105b7624d48d39df91ecff4a
NdirituG <denisndiritug@gmail.com>
2022-04-07 16:02:59 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,12 @@
  * selecta shong'e
  * this one uses puts function
  */
+
 #include <stdio.h>
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }

1d8faf070fa87e34bae78f76e5fac0aa4cd9ae24
NdirituG <denisndiritug@gmail.com>
2022-04-08 09:33:01 -0700

    char numb

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 11; n < 21; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

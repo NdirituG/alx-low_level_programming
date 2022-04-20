1a494bc0a62e7afbf4c4358562a869d15138b6bf
NdirituG <denisndiritug@gmail.com>
2022-04-20 15:31:14 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2.strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - Function that returns the length of a string
+ * @s: String to be check
+ * Return: Length of a string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while(1)
+	{
+		if (*(s + count) == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+
+	return (count);
+}

fe03e9dfbbcbe6f4c0bb1f3e347ad0e52fd6823d
NdirituG <denisndiritug@gmail.com>
2022-04-28 09:40:34 +0300

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+#define MAIN_H_
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);

d133f41b2d3eb43bd4693635121c596e310f1806
NdirituG <denisndiritug@gmail.com>
2022-05-04 01:19:54 +0300

    print number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: unused
  * Return: Always 0 (Success)
  */
-int main(int argc. char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 	printf("%d\n", argc - 1);;
 

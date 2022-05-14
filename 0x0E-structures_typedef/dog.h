d03a2e4868cedea461569970a32ceaf3fe22246c
NdirituG <denisndiritug@gmail.com>
2022-05-14 07:17:39 +0300

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,10 @@
-#ifndef DOG_H
-#define DOG_H
+#define _main_h_
+#define _main_h_
+
+/**
+ * dog - typdef for struct dog
+ */
+typedef struct dog dog_t;
 
 /**
  * struct dog - a dog's basic info
@@ -15,12 +20,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

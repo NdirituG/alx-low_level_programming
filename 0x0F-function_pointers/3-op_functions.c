bfe607e8dc424400d454e51e8c535e23a49c11f5
NdirituG <denisndiritug@gmail.com>
2022-05-16 22:54:18 +0300

    program to perform simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -41,4 +41,31 @@ int op_mul(int a, int b)
  * @a: first number.
  * @b: second number.
  *
+ * Return: division.
+ */
+int op_div(int a , int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
  *
+ * Return: remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}

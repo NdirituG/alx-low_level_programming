5e1cb51adbad65ff4189bf1e1c306ea0cdbcda1b
NdirituG <denisndiritug@gmail.com>
2022-05-24 12:43:45 +0300

    mul and realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _realloc - reallocares a memory block.
+ * _realloc - reallocates a memory block.
  * @ptr: pointer to the memory previously allocated.
  * @old_size: size, in bytes, of the allocated space of ptr.
  * @new_size: new size, in bytes, of the new memory block.
  *
  * Return: ptr.
- * if new size == old_size, returns prt without changes.
+ * if new_size == old_size, returns ptr without changes.
  * if malloc fails, returns NULL.
  */
-void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -19,7 +19,7 @@ void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
-		ptr =  malloc(new_size);
+		ptr = malloc(new_size);
 
 	if (new_size == old_size)
 		return (ptr);
